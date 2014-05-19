7dcec35a0d5353d4880cf3729dbfbea1f16acb7f
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-05-19 12:53:11 -0500

    lints

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,58 +1,51 @@
 /*global define */
 define(['jquery', 'isomer'], function ($, Isomer) {
-    'use strict';
+  'use strict';
 
-    window.requestAnimationFrame = window.requestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.msRequestAnimationFrame;
+  window.requestAnimationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
 
-    function resize() {
-      window.requestAnimationFrame(function() {
-        var main = $('[role=main]'),
-          isomerCanvas = $('#isomer'),
-          w = main.width(),
-          cw = isomerCanvas.width(),
-          ch = isomerCanvas.height(),
-          factor = w / cw;
+  function resize() {
+    window.requestAnimationFrame(function() {
+      var main = $('[role=main]'),
+        isomerCanvas = $('#isomer'),
+        w = main.width(),
+        cw = isomerCanvas.width(),
+        ch = isomerCanvas.height(),
+        factor = w / cw;
 
-        isomerCanvas.width(factor * cw);
-        isomerCanvas.height(factor * ch);
-      });
+      isomerCanvas.width(factor * cw);
+      isomerCanvas.height(factor * ch);
+    });
+  }
+
+  var iso = new Isomer(document.getElementById('isomer'));
+  var angle = 0;
+
+  function scene() {
+    iso.canvas.width = iso.canvas.width;
+    var Shape = Isomer.Shape;
+    var Point = Isomer.Point;
+
+    var background = Shape.Prism(new Point(-1,-1,3));
+    iso.add(background.scale(new Point(-1,-1,3), 3.5, 3.5, -1), new Isomer.Color(192,192,192));
+
+    iso.add(
+    Shape.Prism(new Point(0,0,3)).rotateZ(new Point(0.5,0.5,0), angle), new Isomer.Color(241,74,74));
+    angle += 2 * Math.PI / 270;
+  }
+
+  function draw() {
+    window.requestAnimationFrame(scene);
+  }
+
+  return {
+    init: function() {
+      $(window).on('resize', resize);
+      resize();
+      setInterval(draw);
     }
-
-    var iso = new Isomer(document.getElementById('isomer'));
-    var angle = 0;
-
-    function scene() {
-      iso.canvas.width = iso.canvas.width;
-      var Shape = Isomer.Shape;
-      var Point = Isomer.Point;
-
-      var background = Shape.Prism(new Point(-1,-1,3));
-      iso.add(
-        background.scale(new Point(-1,-1,3), 3.5, 3.5, -1),
-        new Isomer.Color(192,192,192)
-      );
-
-      iso.add(
-        Shape.Prism(new Point(0,0,3))
-        .rotateZ(new Point(0.5,0.5,0), angle),
-        new Isomer.Color(241,74,74)
-      );
-      angle += 2 * Math.PI / 270;
-    }
-
-    function draw() {
-      window.requestAnimationFrame(scene);
-    }
-
-    return {
-      init: function() {
-        $(window).on('resize', resize);
-        resize();
-        setInterval(draw);
-
-      }
-    };
+  };
 });
