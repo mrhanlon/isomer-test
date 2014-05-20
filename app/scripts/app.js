d32c67d9d8fb3947be41c12279dfe5d0c3c3e1ac
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-05-19 22:13:29 -0500

    playing with shapes and rotation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,19 +22,25 @@ define(['jquery', 'isomer'], function ($, Isomer) {
   }
 
   var iso = new Isomer(document.getElementById('isomer'));
-  var angle = 0;
+  var angle = Math.PI/2;
 
   function scene() {
-    iso.canvas.width = iso.canvas.width;
+    iso.canvas.clear();
     var Shape = Isomer.Shape;
     var Point = Isomer.Point;
 
-    var background = Shape.Prism(new Point(-1,-1,3));
-    iso.add(background.scale(new Point(-1,-1,3), 3.5, 3.5, -1), new Isomer.Color(192,192,192));
+    var background = Shape.Cylinder(new Point(0,0,4));
+    iso.add(background.scale(new Point(0,0,0), 2, 2, .5)
+        .rotateZ(new Point(0,0,0), angle)
+      , new Isomer.Color(192,192,192)
+    );
 
-    iso.add(
-    Shape.Prism(new Point(0,0,3)).rotateZ(new Point(0.5,0.5,0), angle), new Isomer.Color(241,74,74));
-    angle += 2 * Math.PI / 270;
+    var prism = Shape.Prism(new Point(0,0,2));
+    iso.add(prism.rotateZ(new Point(0.5,0.5,0), angle), new Isomer.Color(241,74,74));
+    iso.add(prism.translate(0, 0, 1).rotateZ(new Point(0.5,0.5,0), 0.75*angle), new Isomer.Color(241,74,74));
+    iso.add(prism.translate(0, 0, 2).rotateZ(new Point(0.5,0.5,0), 0.50*angle), new Isomer.Color(241,74,74));
+    iso.add(prism.translate(0, 0, 3).rotateZ(new Point(0.5,0.5,0), 0.25*angle), new Isomer.Color(241,74,74));
+    angle += Math.PI / 270;
   }
 
   function draw() {
